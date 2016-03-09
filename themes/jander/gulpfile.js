0951150e0feec9fbc7f0490ee13685b58c713cb8
Jon Anderson <jon.david.anderson@gmail.com>
2016-03-09 15:03:56 -0800

    Fixed index page. Added temporary post page.

--- themes/jander/gulpfile.js
+++ themes/jander/gulpfile.js
@@ -1,33 +1,44 @@
-var gulp = require('gulp'),
+var g = require('gulp'),
     $ = require('gulp-load-plugins')(),
-    bower = require('bower');
+    b = require('bower');
 
-gulp.task('bower', () => {
-    bower.commands.install();   
+// Configuration
+// =============
+
+var jsFiles = [
+    'bower_components/foundation-sites/dist/foundation.js',
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/what-input/what-input.js',
+    'bower_components/jQuery-Flex-Vertical-Center/jquery.flexverticalcenter.js'
+];
+var sassPaths = [
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
+];
+
+// Tasks
+// =====
+
+g.task('bower', () => {
+    b.commands.install();   
 });
 
-gulp.task('js', ['bower'], () => {
-    return gulp.src([
-            'bower_components/foundation-sites/dist/foundation.js',
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/what-input/what-input.js'
-        ])
-        .pipe(gulp.dest('source/js'));
+g.task('js', ['bower'], () => {
+    return g.src(jsFiles)
+        .pipe(g.dest('source/js/lib'));
 });
 
-gulp.task('sass', ['bower'], () => {
-    return gulp.src('source/_scss/app.scss')
+g.task('sass', ['bower'], () => {
+    return g.src('source/_scss/foundation-app.scss')
         .pipe($.sass({
-            includePaths: [
-                'bower_components/foundation-sites/scss',
-                'bower_components/motion-ui/src'
-            ]
+            includePaths: sassPaths
         }))
         .on('error', $.sass.logError)
         .pipe($.autoprefixer({
             browsers: ['last 2 versions', 'ie >= 9']
         }))
-        .pipe(gulp.dest('source/css'));
+        .pipe($.rename('foundation.css'))
+        .pipe(g.dest('source/css/lib'));
 });
 
-gulp.task('default', ['js', 'sass']);
+g.task('default', ['js', 'sass']);
--- themes/jander/source/js/index.js
+++ themes/jander/source/js/index.js
@@ -1,113 +1,138 @@
 // Menu
+// ====
+
 (function ($) {
-	// Returns a random value between min and max
-	function getValue (min, max) {
-		return min + ((max - min) * Math.random());
-	}
+	// Configuration
+	// =============
 
-	// Swaps the picture
-	function swap (new_pic_path) {
-		$("#menu_face").attr("src", new_pic_path);
-	}
+	var imagePaths = {
+		// The default idling image
+		main: 'assets/face/idle1.jpg',
 
-	// Called after every cooldown to display an alternative pic
-	function running () {
-		var cool_time = cooldown.get();
-		var disp_time = display.get();
-		var alt = map.get();
-
-		// Swap out with alt pic
-		swap(alt.path);
-
-		// Schedule picture to revert back
-		disp_timer_id = setTimeout(function () {
-			swap(pics.main);
-
-			// After picture has reverted, schedule the next time for it to run
-			cool_timer_id = setTimeout(running, cool_time);
-		}, disp_time);
-	}
-
-	var disp_timer_id = -1;
-	var cool_timer_id = -1;
-
-	var pics = {
-		// The default idling picture
-		main: "img/face/idle1.jpg",
-
-		// Alternative pics that are randomly chosen (with weights) and displayed for a couple seconds
+		// Alternative images that are randomly chosen (with weights) and 
+		// displayed for a couple seconds
 		alts: [
 			{
-				path: "img/face/idle2.jpg",
+				path: 'assets/face/idle2.jpg',
 				weight: 2
 			},
 			{
-				path: "img/face/idle3.jpg",
+				path: 'assets/face/idle3.jpg',
 				weight: 1
 			}
 		],
 
+		// Button images are displayed when the mouse hovers over a
+		// button.
 		buttons: {
-			button1: "img/face/button1.jpg",
-			button2: "img/face/button2.jpg",
-			button3: "img/face/button3.jpg",
-			button4: "img/face/button4.jpg",
-			button5: "img/face/button5.jpg"
+			button1: 'assets/face/button1.jpg',
+			button2: 'assets/face/button2.jpg',
+			button3: 'assets/face/button3.jpg',
+			button4: 'assets/face/button4.jpg',
+			button5: 'assets/face/button5.jpg'
 		}
-	};
+	}
 
-	// The range of elapse time between displaying the alternative pics
-	var cooldown = {
-		min: 2,
-		max: 5,
-		get: function () {
-			return getValue(cooldown.min, cooldown.max) * 1000;
+		// The range of time between displaying
+		// alt images.
+	var cooldownMin = 2000,
+		cooldownMax = 5000,
+
+		// The range of time an alt image is displayed.
+		displayMin = 500,
+		displayMax = 2000
+
+
+	// Functions
+	// =========
+
+	// Returns a random number generator.
+	var createNumGen = function (min, max) {
+		return function () {
+			return min + ((max - min) * Math.random())
 		}
-	};
+	}
 
-	// The range of time an alternative pic is displayed for
-	var display = {
-		min: 0.5,
-		max: 2,
-		get: function () {
-			return getValue(display.min, display.max) * 1000;
-		}
-	};
+	var swapImage = function (new_pic_path) {
+		$('#face').attr('src', new_pic_path)
+	}
 
-	// The weighted mapping table
-	var map = {
+	// Starts the loop that displays the alternative images.
+	var startLoop = function () {
+		cooldownTimerId = setTimeout(function loop () {
+			var cool_time = getCooldown(),
+				disp_time = getDisplay(),
+				alt = altImages.get()
+
+			// swapImage out with alt pic
+			swapImage(alt)
+
+			// Schedule picture to revert back
+			displayTimerId = setTimeout(function ()  {
+				swapImage(imagePaths.main)
+
+				// After picture has reverted, schedule the next time for it to run
+				cooldownTimerId = setTimeout(loop, cool_time)
+			}, disp_time)
+		}, getCooldown())
+	}
+
+	// Stop the loop that displays the alternative images.
+	var stopLoop = function () {
+		clearTimeout(displayTimerId)
+		clearTimeout(cooldownTimerId)
+	}
+
+
+	// Initialize
+	// ==========
+
+	var displayTimerId = -1,
+		cooldownTimerId = -1,
+		getCooldown = createNumGen(cooldownMin, cooldownMax),
+		getDisplay = createNumGen(displayMin, displayMax)
+
+	// The weighted table used to choose an
+	// alt image.
+	var altImages = {
 		table: [],
 		get: function () {
-			var ticks = 1 / map.table.length;
-			var i = Math.floor(Math.random() / ticks);
-			return map.table[i];
+			var ticks = 1 / altImages.table.length
+			var i = Math.floor(Math.random() / ticks)
+			return altImages.table[i]
 		}
-	};
+	}
 
-	// Build the mapping table
-	$.each(pics.alts, function () {
-		for (var i=0; i<this.weight; i++) {
-			map.table.push(this);
+	// Build the alt images table
+	imagePaths.alts.forEach((alt) => {
+		for (var i=0; i<alt.weight; i++) {
+			altImages.table.push(alt.path)
 		}
-	});
+	})
 
-	// Schedule running to be called at a random cooldown time
-	cool_timer_id = setTimeout(running, cooldown.get());
 
-	// Register callbacks for mouseover mouseoff events
-	$(document).ready(function () {
-		$("#button1, #button2, #button3, #button4, #button5").hover(function () { // mouseover callback
-			// Stop any other timers
-			clearTimeout(disp_timer_id);
-			clearTimeout(cool_timer_id);
+	// Attach to DOM
+	// =============
 
-			var id = $(this).attr("id");
-			swap(pics.buttons[id]);
-		}, function () { // mouseoff callback
-			swap(pics.main);
-			cool_timer_id = setTimeout(running, cooldown.get());
-		}).click(function () { // mouse click handler
-			$("#button1, #button2, #button3, #button4, #button5").unbind('mouseenter mouseleave click');
-		});
-	});
-})(jQuery);
+	startLoop()
+
+	// Register mouse callbacks
+	$(function () {
+		$('#button1, #button2, #button3, #button4, #button5')
+		.hover(function () { // mouseover
+			stopLoop()
+			var id = $(this).attr('id')
+			swapImage(imagePaths.buttons[id])
+		},
+		function () { // mouseoff
+			swapImage(imagePaths.main)
+			startLoop()
+		})
+		.click(function () {
+			$('#button1, #button2, #button3, #button4, #button5')
+			.unbind('mouseenter mouseleave click')
+		})
+
+		$('#menu').flexVerticalCenter();
+	})
+})(jQuery)
