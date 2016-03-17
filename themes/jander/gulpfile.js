ec877b832c9f121e380a0f2b50034d76d9f47dda
chronzerg <jon.david.anderson@gmail.com>
2016-03-16 21:43:18 -0700

    Added common and specific javascript and css folder.

--- themes/jander/gulpfile.js
+++ themes/jander/gulpfile.js
@@ -1,92 +1,91 @@
 var g = require('gulp'),
     $ = require('gulp-load-plugins')(),
-    bowerFiles = require('main-bower-files')
+    bowerFiles = require('main-bower-files'),
+    _ = require('lodash');
 
 
 // Configuration
 // =============
 
-// External JS Libraries
-var jsLibs = bowerFiles(/.*\.js/);
+var js = {
+    libaries: bowerFiles(/.*\.js/),
+    output:   'source/js/lib'
+};
 
-// Foundation Sass
-var fndEntry = 'source/_scss/foundation-entry.scss',
-    fndSettings = 'source/_scss/foundation-settings.scss';
-
-// Jander Sass
-var janderSass = 'source/_scss/jander.scss';
-
-// Sass Includes
-var sassInc = [
-    'bower_components/foundation-sites/scss'
-];
-
-var iconFiles = 'bower_components/foundation-icon-fonts/foundation-icons.*';
-
-// Destinations
-var jsDst = 'source/js/lib',
-    cssDst = 'source/css';
+var css = {
+    jander: {
+        common:   'source/_scss/app.scss',
+        specific: 'source/_scss/!(app).scss'
+    },
+    foundation: {
+        entry:    'source/_scss/foundation/entry.scss',
+        settings: 'source/_scss/foundation/settings.scss',
+        icons:    'bower_components/foundation-icon-fonts/foundation-icons.*',
+        includes: 'bower_components/foundation-sites/scss'
+    },
+    output: {
+        common:   'source/css/common',
+        specific: 'source/css'
+    }
+};
 
 
 // JS Tasks
 // ========
 
+// Copy JavaScript libraries into the the output folder.
 g.task('js', () => {
-    return g.src(jsLibs)
-        .pipe(g.dest(jsDst));
+    return g.src(js.libaries).pipe(g.dest(js.output));
 });
 
 
 // SASS Tasks
 // ==========
 
-g.task('sass-foundation', () => {
-    return g.src(fndEntry)
+function compileSass (gulpSrc) {
+    return gulpSrc
         .pipe($.sass({
-            includePaths: sassInc
+            includePaths: css.foundation.includes
         }))
         .on('error', $.sass.logError)
         .pipe($.autoprefixer({
             browsers: ['last 2 versions', 'ie >= 9']
-        }))
+        }));
+}
+
+g.task('css-specific', () => {
+    return compileSass(g.src(css.jander.specific))
+        .pipe(g.dest(css.output.specific));
+});
+
+g.task('css-common', () => {
+    return compileSass(g.src(css.jander.common))
+        .pipe(g.dest(css.output.common));
+});
+
+g.task('css-foundation', () => {
+    return compileSass(g.src(css.foundation.entry))
         .pipe($.rename('foundation.css'))
-        .pipe(g.dest(cssDst));
+        .pipe(g.dest(css.output.common));
 });
 
-g.task('sass-jander', () => {
-    return g.src(janderSass)
-        .pipe($.sass({
-            includePaths: sassInc
-        }))
-        .on('error', $.sass.logError)
-        .pipe($.autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }))
-        .pipe(g.dest(cssDst));
+g.task('css-icons', () => {
+    return g.src(css.foundation.icons).pipe(g.dest(css.output.common));
 });
 
-g.task('sass', ['sass-foundation', 'sass-jander']);
-
-
-// Icon Tasks
-// ==========
-
-g.task('icons', () => {
-    return g.src(iconFiles)
-        .pipe(g.dest(cssDst));
-});
+g.task('css', ['css-specific', 'css-common', 'css-foundation', 'css-icons']);
 
 
 // Clean Tasks
 // ===========
 
 g.task('clean-js', () => {
-    return g.src(jsDst)
+    return g.src(js.output)
         .pipe($.clean());
 });
 
 g.task('clean-css', () => {
-    return g.src(cssDst)
+    return g.src(_.toArray(css.output))
         .pipe($.clean());
 });
 
@@ -97,14 +96,21 @@ g.task('clean', ['clean-js', 'clean-css']);
 // ===========
 
 g.task('watch', ['default'], () => {
-    g.watch(jsLibs, ['js']);
-    g.watch([fndEntry, fndSettings], ['sass-foundation']);
-    g.watch(janderSass, ['sass-jander']);
-    g.watch(iconFiles, ['icons']);
+    g.watch([
+            css.foundation.settings,
+            css.jander.specific
+        ], ['css-specific']);
+
+    g.watch([
+            css.foundation.settings,
+            css.jander.common
+        ], ['css-common']);
+
+    g.watch([css.foundation.settings], ['css-foundation']);
 });
 
 
 // Default Task
 // ============
 
-g.task('default', ['js', 'sass', 'icons']);
+g.task('default', ['js', 'css']);
