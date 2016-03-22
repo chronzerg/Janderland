e433472ad7b0b2865e1e138454ae0bd1a1c89e6f
chronzerg <jon.david.anderson@gmail.com>
2016-03-21 19:11:34 -0700

    Restructured the CSS build system.

--- themes/jander/gulpfile.js
+++ themes/jander/gulpfile.js
@@ -20,37 +20,15 @@ var js = {
 };
 
 var css = {
-    jander: {
-        common:   'source/_scss/app.scss',
-        specific: 'source/_scss/!(app).scss'
-    },
-    foundation: {
-        entry:    'source/_scss/foundation/entry.scss',
-        settings: 'source/_scss/foundation/settings.scss',
-        icons:    'bower_components/foundation-icon-fonts/foundation-icons.*',
-        includes: 'bower_components/foundation-sites/scss'
-    },
-    output: {
-        common:   'source/css/common',
-        specific: 'source/css',
-        ejs:      'layout/_generated/common_css.ejs'
-    }
+    input: '_scss/app.scss',
+    icons: 'bower_components/foundation-icon-fonts/foundation-icons.*',
+    includes: 'bower_components/foundation-sites/scss',
+    output: 'source'
 };
 
 
 // Helpers
 // =======
-
-function compileSass (gulpSrc) {
-    return gulpSrc
-        .pipe($.sass({
-            includePaths: css.foundation.includes
-        }))
-        .on('error', $.sass.logError)
-        .pipe($.autoprefixer({
-            browsers: ['last 2 versions', 'ie >= 9']
-        }));
-}
 
 function toFileList (gulpSrc) {
     return stream2Array(gulpSrc)
@@ -86,35 +64,23 @@ g.task('js', ['js-libraries', 'js-ejs']);
 // CSS Tasks
 // ==========
 
-g.task('css-specific', () => {
-    return compileSass(g.src(css.jander.specific))
-        .pipe(g.dest(css.output.specific));
-});
-
-g.task('css-common', () => {
-    return compileSass(g.src(css.jander.common))
-        .pipe(g.dest(css.output.common));
-});
-
-g.task('css-foundation', () => {
-    return compileSass(g.src(css.foundation.entry))
-        .pipe($.rename('foundation.css'))
-        .pipe(g.dest(css.output.common));
+g.task('css-app', () => {
+    return g.src(css.input)
+        .pipe($.sass({
+            includePaths: css.includes
+        }))
+        .on('error', $.sass.logError)
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(g.dest(css.output));
 });
 
 g.task('css-icons', () => {
     return g.src(css.foundation.icons).pipe(g.dest(css.output.common));
 });
 
-g.task('css-ejs', ['css-specific', 'css-common', 'css-foundation', 'css-icons'], () => {
-    return toFileList(g.src(css.output.common + '/*.css'))
-    .then((files) => {
-        var data = '<%- css(' + JSON.stringify(files, null, 4) + ') %>';
-        return fs.writeFile(css.output.ejs, data);
-    });
-});
-
-g.task('css', ['css-specific', 'css-common', 'css-foundation', 'css-icons', 'css-ejs']);
+g.task('css', ['css-app', 'css-icons']);
 
 
 // Clean Tasks
@@ -126,7 +92,7 @@ g.task('clean-js', () => {
 });
 
 g.task('clean-css', () => {
-    return g.src(_.toArray(css.output))
+    return g.src(css.output + '/app.css')
         .pipe($.clean());
 });
 
